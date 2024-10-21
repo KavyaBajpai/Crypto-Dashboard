eaa2ef8d3f87b8482c9369496267c4c3b40723aa
KavyaBajpai <kavyabajpai05@gmail.com>
2024-10-21 23:57:12 +0530

    added comments

--- src/pages/Dashboard/components/PortfolioSection.jsx
+++ src/pages/Dashboard/components/PortfolioSection.jsx
@@ -4,6 +4,7 @@ function PortfolioSection() {
   return (
     <div>
       {/* //this section holds the sections of the dashboard. */}
+      //yeah this is for just getting in the contribution... nothing to see here...
        
     </div>
   )
