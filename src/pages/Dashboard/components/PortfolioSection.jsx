a7d819df6d3067c7be577d288978c36b43ef754d
KavyaBajpai <kavyabajpai05@gmail.com>
2024-10-21 23:54:33 +0530

    added comments

--- src/pages/Dashboard/components/PortfolioSection.jsx
+++ src/pages/Dashboard/components/PortfolioSection.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function PortfolioSection() {
   return (
     <div>
-      
+      {/* //this section holds the sections of the dashboard. */}
+       
     </div>
   )
 }
